8b11493631eee5004bc1b1a6f615b7f7be295e99
AtakltiHanis <atakltihanis14@gmail.com>
2025-01-17 00:26:52 +0300

    to the final

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,7 @@
 import emailjs from "@emailjs/browser";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import { FiSend } from "react-icons/fi";
 const ContactForm = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -8,10 +9,10 @@ const ContactForm = () => {
     message: "",
   });
   const [errors, setErros] = useState({});
-  const [sending, setIsSending] = useState(false);
+  const [isSending, setIsSending] = useState(false);
 
   const handleChange = (e) => {
-    const [name, value] = e.target;
+    const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
   const validate = () => {
@@ -19,7 +20,7 @@ const ContactForm = () => {
     if (!formData.name) errors.name = "Name is required";
     if (!formData.email) {
       errors.email = "Email is required";
-    } else if (!/\S+ @\S+\.\S+/.test(formData.email)) {
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
       errors.email = "Email is invalid";
     }
     if (!formData.message) errors.message = "Message is Required";
@@ -60,6 +61,64 @@ const ContactForm = () => {
       <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">
         Let's Connect
       </h2>
+      <form action="" onSubmit={handleSubmit}>
+        <div className="mb-4 flex space-x-4">
+          <div className="lg:w-1/2">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              placeholder="Name"
+              onChange={handleChange}
+              className="mb-8 w-full appearance-none rounded-lg border border-stone-50/30 bg-transparent px-3 py-2 text-sm focus:border-stone-400 focus:outline-none"
+            />
+            {errors.name && (
+              <p className="text-sm text-rose-800">{errors.name}</p>
+            )}
+          </div>
+          <div className="lg:w-1/2">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              placeholder="Email"
+              onChange={handleChange}
+              className="mb-8 w-full appearance-none rounded-lg border border-stone-50/30 bg-transparent px-3 py-2 text-sm focus:border-stone-400 focus:outline-none"
+            />
+            {errors.email && (
+              <p className="text-sm text-rose-800">{errors.email}</p>
+            )}
+          </div>
+        </div>
+        <div className="lg:w-1/2">
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Message"
+            value={formData.message}
+            onChange={handleChange}
+            className="mb-8 w-full appearance-none rounded-lg border border-stone-50/30 bg-transparent px-3 py-2 text-sm focus:border-stone-400 focus:outline-none"
+            rows="6"
+          />
+          {errors.message && (
+            <p className="text-sm text-rose-800">{errors.message}</p>
+          )}
+        </div>
+        <button
+          type="submit"
+          className={`mb-8 w-full rounded border border-stone-50/30 bg-stone-200 px-4 py-2 text-sm font-semibold text-stone-900 hover:bg-stone-300 ${
+            isSending ? "cursonr-not-allowed opacity-50" : ""
+          }`}
+          disabled={isSending}
+        >
+          <div className="flex items-center justify-center gap-2">
+            {isSending ? "Sending" : "Send"}
+            <FiSend />
+          </div>
+        </button>
+      </form>
     </div>
   );
 };
